8df763b573e2e3727b8640952c299adb71ec1e7e
edison cardenas <edisoncardenas151999@yahooo.com>
2022-09-04 12:18:55 -0400

    done

--- index.js
+++ index.js
@@ -17,7 +17,59 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // iteration 2
+
+    let newRecipe = {
+      title: 'miXto quente',
+      level: 'Easy Peasy',
+      ingredients: ['pão francês', 'queijo', 'presunto'],
+      cuisine: 'Brasileira',
+      dishType: 'snack',
+      image:
+        'http://culinaria.culturamix.com/blog/wp-content/gallery/misto-quente-3/Misto-Quente-6.jpg',
+      duration: 5,
+      creator: 'unknown'
+    };
+
+
+    return Recipe.create(newRecipe)
+  })
+  .then(result => console.log(`recipe added: ${result.title}`))
+  // iteration 3
+
+  // here we are passing an array of objects to the ".insertMany()" method that literally does what it says:
+  // inserts multiple objects into the database as documents of the "recipes" collection
+  .then(() => Recipe.insertMany(data))
+  .then(result => {
+    result.forEach(item => {
+        console.log(`recipe for ${item.title} inserted successfully`);
+      })
+      // iteration 4
+
+      // under the hood, ".updateOne()" basically works as ".findOneAndUpdate()" method:
+      // finds a certain document in the database based on some criteria (in this case based on the "title"),
+      // and sets some of the properties to the new values and saves it in the database
+      return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, {new: true})
+  })
+  .then(result => {
+    console.log(`Updated ${result.title} and new duration is: ${result.duration}`)
+
+    // iteration 5
+
+    // under the hood, it works as ".findOneAndDelete" method - find doc based on some criteria (in this case title but could be any other property)
+    // and removes it from the DB permanently
+    return Recipe.deleteOne({ title: 'Carrot Cake' })
+  })
+  .then(() => {
+    console.log(`The recipe is deleted`)
+    return Recipe.deleteOne({ title: 'Orange and Milk-Braised Pork Carnitas' })
+  })
+  .then(() => {
+    console.log(`The recipe is deleted`)
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    console.error('Error: ', error);
+  })
+  // iteration 6
+
+  // alternative way to using ".close()" method is using ".disconnect()". The best practice is to use ".close()" but you will see sometimes the alternative way as well
